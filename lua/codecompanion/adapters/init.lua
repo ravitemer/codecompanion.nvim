ravitemer/codecompanion.nvim

commit 0beb9183954ae1afa2e1f5a91812706764d1a743

fix(adapters): resolve adapters with correct name (#1459)

lua/codecompanion/adapters/init.lua
@@ -316,12 +316,14 @@ end
 ---@param adapter? CodeCompanion.Adapter|string|function
 ---@return CodeCompanion.Adapter
 function Adapter.resolve(adapter)
-  adapter = adapter or config.adapters[config.strategies.chat.adapter]
+  adapter = adapter or config.strategies.chat.adapter
 
   if type(adapter) == "table" then
     adapter = Adapter.new(adapter)
   elseif type(adapter) == "string" then
+    local name = adapter
     adapter = Adapter.extend(adapter)
+    adapter.name = name
   elseif type(adapter) == "function" then
     adapter = adapter()
   end

commit 82df942881a03530a1a6114c8e90b5dd97377527

fix: adapters.resolve not using custom adapter, bug introduced with (#1459)

lua/codecompanion/adapters/init.lua
@@ -322,7 +322,8 @@ function Adapter.resolve(adapter)
     adapter = Adapter.new(adapter)
   elseif type(adapter) == "string" then
     local name = adapter
-    adapter = Adapter.extend(adapter)
+    local user_adapter = config.adapters[name]
+    adapter = Adapter.extend(user_adapter or adapter)
     adapter.name = name
   elseif type(adapter) == "function" then
     adapter = adapter()